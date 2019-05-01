3ea1e88f07c5f101ea0d3a77538e749d2323a66c
Artem Bebenin <artjombebenin@gmail.com>
2019-05-01 14:03:18 +0300

    init view-main

---
+++ src/views/index.js
@@ -0,0 +1,5 @@
+//
+import mainView from '@/views/view-main'
+export const main = mainView
+//
+// import {  } from "module";
---
+++ src/views/view-main.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    main-view
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'view-main',
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
