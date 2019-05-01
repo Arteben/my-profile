8477e83baa7af2003212e1b62fa712e7f114367b
Artem Bebenin <artjombebenin@gmail.com>
2019-05-01 12:54:35 +0300

    add webpack aliases

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
-import './plugins/vuetify'
-import App from './App.vue'
-import router from './router'
+import '@/vuetify'
+import App from '@/App.vue'
+import router from '@/router'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import '@mdi/font/css/materialdesignicons.css'
 
