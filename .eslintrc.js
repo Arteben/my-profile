bb54a8caafa8ca31eeb232a45f964a51c2b604b2
Artem Bebenin <artjombebenin@gmail.com>
2019-05-03 11:36:44 +0300

    set configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,25 @@ module.exports = {
     node: true
   },
   'extends': [
-    'plugin:vue/essential',
+    'plugin:vue/strongly-recommended',
     'eslint:recommended'
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-console': 'off',
+    'no-debugger': 'off',
+
+    'no-trailing-spaces': 'error',
+    'quotes': ['error', 'single'],
+
+    'vue/array-bracket-spacing': 'error',
+    'vue/arrow-spacing': 'error',
+    'vue/block-spacing': 'error',
+    'vue/brace-style': 'error',
+    'vue/comma-dangle': 'error',
+    'vue/key-spacing': 'error',
+    'vue/match-component-file-name': 'error',
+    'vue/object-curly-spacing': 'error',
+    'vue/script-indent': 'error'
   },
   parserOptions: {
     parser: 'babel-eslint'
