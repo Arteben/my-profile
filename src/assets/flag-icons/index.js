3c92bc9a41665d860a999619d22584bc78226ab2
artem <artjombebenin@gmail.com>
2022-04-01 14:50:16 +0300

    MP-10 add files for translates

---
+++ src/assets/flag-icons/index.js
@@ -0,0 +1,2 @@
+export { default as ruIcon } from './RU.svg'
+export { default as engIcon } from './GB.svg'
---
+++ src/components/flag-icon.vue
@@ -0,0 +1,39 @@
+<template>
+  <img
+    :src="icon"
+    :class="$style.icon"
+    :width="width"
+    :height="width"
+  >
+</template>
+
+<script>
+import * as icons from '@/assets/flag-icons';
+
+export default {
+  name: 'FlagIcon',
+  data() {
+    return {
+      icons,
+      width: 35,
+    };
+  },
+  computed: {
+    lang () {
+      // const lang = this.getLang()
+      // return lang == 'ru' && 'eng' || 'ru'
+      return 'ru'
+    },
+    icon () {
+      return this.icons[`${this.lang}Icon`]
+    },
+  },
+};
+</script>
+
+<style module>
+.icon {
+  border: 4px solid var(--v-primaryText-base);
+  border-radius: 30px;
+}
+</style>
