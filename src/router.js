26d586718bae2734bb11e3937a0909b994ec9a0e
Artjom <artjombebenin@gmail.com>
2025-03-21 15:48:15 +0400

    fixed routes for print

--- src/router.js
+++ src/router.js
@@ -36,7 +36,7 @@ export default new Router({
       component: app,
     },
     {
-      path: '/print/:lang/',
+      path: '/:lang//print/',
       name: 'print',
       component: printView,
     },
